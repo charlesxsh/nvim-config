ce33d02fee3197242a3e4ca85718f3eca3013c74
Shihao Xia <charlesxsh@hotmail.com>
2023-01-08 03:22:33 -0500

    add clangd as default\ntemp fix for mason-lsp ssh issue

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,7 +1,8 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 lsp.ensure_installed({
-'sumneko_lua',
-'rust_analyzer',
+	'sumneko_lua',
+	'rust_analyzer',
+	'clangd',
 })
 lsp.setup()
---
+++ after/plugin/mason.lua
@@ -0,0 +1,6 @@
+require("mason").setup {
+    providers = {
+        "mason.providers.client",
+        -- "mason.providers.registry-api" -- This is the default provider. You can still include it here if you want, as a fallback to the client provider.
+    }
+}
